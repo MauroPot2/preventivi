5c0a7c89eeaee9f7b6a35adb68ee901c57137f34
Mauro Leonardo Potestio <mauropot@Host-007.homenet.telecomitalia.it>
2026-09-17 19:08:40 +0200

    add work item domain

--- lib/features/catalog/domain/repositories/work_item_repository.dart
+++ lib/features/catalog/domain/repositories/work_item_repository.dart
@@ -1,1 +1,13 @@
+import 'package:preventiviedili/features/catalog/domain/work_item.dart';
 
+abstract class WorkItemRepository {
+  Future<List<WorkItem>> getAllWorkItems();
+
+  Future<WorkItem?> getWorkItemById(String id);
+
+  Future<WorkItem> createWorkItem(WorkItem workItem);
+
+  Future<void> updateWorkItem(WorkItem workItem);
+
+  Future<void> deleteWorkItem(String id);
+}
--- lib/features/catalog/domain/work_item.dart
+++ lib/features/catalog/domain/work_item.dart
@@ -1,1 +1,17 @@
+import 'package:preventiviedili/core/domain/unit_measurement.dart';
 
+class WorkItem {
+  final String id;
+  final String name;
+  final String? description;
+  final double basePrice;
+  final UnitMeasurement unitMeasurement;
+
+  WorkItem({
+    required this.id,
+    required this.name,
+    this.description,
+    required this.basePrice,
+    required this.unitMeasurement,
+  });
+}
