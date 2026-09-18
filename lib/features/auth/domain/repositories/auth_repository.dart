b682e6397fa8250f3f03533d315037384cc84574
Mauro Leonardo Potestio <mauropot@Host-007.homenet.telecomitalia.it>
2026-09-18 07:55:17 +0200

    add auth domain

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,1 +1,13 @@
+import 'package:preventiviedili/features/auth/domain/user.dart';
 
+abstract class AuthRepository {
+  Future<User?> getCurrentUser();
+
+  Future<User> login(String email, String password);
+
+  Future<User> registrationUser(String name, String email, String password);
+
+  Future<void> logout();
+
+  Future<void> resetPassword(String email);
+}
